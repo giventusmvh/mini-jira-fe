85a6b2be465b9fd0d98718cb2b875d755b721f46
Giventus Marco <weishen735@gmail.com>
2026-01-21 20:44:18 +0700

    feat: Implement reactive forms for login and registration pages with validation and facade integration.

--- src/app/features/auth/pages/login.ts
+++ src/app/features/auth/pages/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthFacade } from '../facades/auth.facade';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Login {
+
+  private readonly fb = inject(FormBuilder);
+  protected readonly facade = inject(AuthFacade);
+
+  //reactive form dengan validasi
+  protected readonly loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit(): void{
+    if(this.loginForm.valid){
+      const{email, password} = this.loginForm.value;
+      this.facade.login({
+        email: email!,
+        password: password!
+      })
+    }
+  }
 
 }
--- src/app/features/auth/pages/register.ts
+++ src/app/features/auth/pages/register.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthFacade } from '../facades/auth.facade';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+
+  private readonly fb = inject(FormBuilder)
+  protected readonly facade = inject(AuthFacade)
+
+  protected readonly registerForm = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  onSubmit(): void{
+    if(this.registerForm.valid){
+      const{name,email,password} = this.registerForm.value;
+      this.facade.register({
+        name: name!,
+        email: email!,
+        password: password!
+      })
+    }
+  }
 
 }
